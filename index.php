f2f14b4226fbf09c219cfb7de64ca54a78bdc2d2
Warren Harper <warrenjharper@gmail.com>
2013-11-18 17:45:34 -0500

    Initial work on the frontend

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+      <head>
+            <title>Mochila</title>
+            <?php include("header.inc"); ?>
+      </head>
+      <body>
+            
+            <?php include("navbar.html"); ?>
+            
+            
+            <div id="content" class="container">
+                  <div id="dagr-list" class="list-group">
+                        <div class="media list-group-item">
+                              <a class="pull-left" href="#">
+                                    <img class="media-object" src="images/mochila.png" alt="Mochila"/></a>
+                              <span class="media-body">
+                                    <h4 class="media-heading">Media Heading</h4>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </span>
+                              <div href="#" class="pull-right" >
+                                    <div class="btn-group">
+                                          <button type="button" class="btn btn-default">
+                                                <div class="glyphicon glyphicon-tag"></div>
+                                                <div><h6>add tag</h6></div>
+                                          </button>
+                                          <button type="button" class="btn btn-default">
+                                                <div class="glyphicon glyphicon-trash"></div>
+                                                <div><h6>delete</h6></div>
+                                          </button>
+                                    
+                                    </div>
+                                    </div>
+                              
+                        </div>
+                         <div class="media list-group-item">
+                              <a class="pull-left" href="#">
+                                    <img class="media-object" src="images/mochila.png" alt="Mochila"/></a>
+                              <span class="media-body">
+                                    <h4 class="media-heading">Media Heading</h4>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </span>
+                              <div href="#" class="pull-right" >
+                                    <div class="btn-group">
+                                          <button type="button" class="btn btn-default">
+                                                <div class="glyphicon glyphicon-tag"></div>
+                                                <div><h6>add tag</h6></div>
+                                          </button>
+                                          <button type="button" class="btn btn-default">
+                                                <div class="glyphicon glyphicon-trash"></div>
+                                                <div><h6>delete</h6></div>
+                                          </button>
+                                    
+                                    </div>
+                                    </div>
+                              
+                        </div>
+                         <div class="media list-group-item">
+                              <a class="pull-left" href="#">
+                                    <img class="media-object" src="images/mochila.png" alt="Mochila"/></a>
+                              <span class="media-body">
+                                    <h4 class="media-heading">Media Heading</h4>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </span>
+                              <div href="#" class="pull-right" >
+                                    <div class="btn-group">
+                                          <button type="button" class="btn btn-default">
+                                                <div class="glyphicon glyphicon-tag"></div>
+                                                <div><h6>add tag</h6></div>
+                                          </button>
+                                          <button type="button" class="btn btn-default">
+                                                <div class="glyphicon glyphicon-trash"></div>
+                                                <div><h6>delete</h6></div>
+                                          </button>
+                                    
+                                    </div>
+                                    </div>
+                              
+                        </div>
+                         <div class="media list-group-item">
+                              <a class="pull-left" href="#">
+                                    <img class="media-object" src="images/mochila.png" alt="Mochila"/></a>
+                              <span class="media-body">
+                                    <h4 class="media-heading">Media Heading</h4>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </span>
+                              <div href="#" class="pull-right" >
+                                    <div class="btn-group">
+                                          <button type="button" class="btn btn-default">
+                                                <div class="glyphicon glyphicon-tag"></div>
+                                                <div><h6>add tag</h6></div>
+                                          </button>
+                                          <button type="button" class="btn btn-default">
+                                                <div class="glyphicon glyphicon-trash"></div>
+                                                <div><h6>delete</h6></div>
+                                          </button>
+                                    
+                                    </div>
+                                    </div>
+                              
+                        </div>
+                         <div class="media list-group-item">
+                              <a class="pull-left" href="#">
+                                    <img class="media-object" src="images/mochila.png" alt="Mochila"/></a>
+                              <span class="media-body">
+                                    <h4 class="media-heading">Media Heading</h4>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </span>
+                              <div href="#" class="pull-right" >
+                                    <div class="btn-group">
+                                          <button type="button" class="btn btn-default">
+                                                <div class="glyphicon glyphicon-tag"></div>
+                                                <div><h6>add tag</h6></div>
+                                          </button>
+                                          <button type="button" class="btn btn-default">
+                                                <div class="glyphicon glyphicon-trash"></div>
+                                                <div><h6>delete</h6></div>
+                                          </button>
+                                    
+                                    </div>
+                                    </div>
+                              
+                        </div>
+                        
+                        
+                        
+                  </div>  
+            </div>
+            
+            <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+      </body>
+</html>
