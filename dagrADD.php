103366fcfaafb2bef020b197b55d7234fac38e9f
Stephen <stephen@stephen-VirtualBox.(none)>
2013-12-04 18:32:38 -0500

    edited dagrADD.php to echo all parameters of GET request

--- dagrADD.php
+++ dagrADD.php
@@ -2,6 +2,6 @@
 $targetURL = $_GET['q'];
 $dagrTitle = $_GET['title'];
 $dagrTags = $_GET['tags'];
-$responseMessage = "Received URL: ", $targetURL, "\nDAGR Title: ", $dagrTitle, "\nDAGR Tags: ", $dagrTags, "\n";
+$responseMessage = "Received URL: $targetURL\nDAGR Title: $dagrTitle\nDAGR Tags: $dagrTags\n";
 echo $responseMessage;
 ?>
