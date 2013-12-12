65384ba573cee23e97f20e1c66286d1a37857878
Warren Harper <warrenjharper@gmail.com>
2013-12-12 15:53:44 -0500

    Backend dagr deletion finished and tested

--- dagrDelete.php
+++ dagrDelete.php
@@ -1,31 +1,48 @@
 <?php 
+
+
+function singular_delete($guid, $db){
+    $children_statement = $db->prepare("update `DAGRS` set `DAGR_PARENT_GUID`=NULL where `DAGR_PARENT_GUID`= ?");
+    $dagr_statement = $db->prepare("delete from `DAGRS` where `DAGR_GUID` = ?");
+    $children_statement->bind_param("s", $guid);
+    $dagr_statement->bind_param("s", $guid);
+    $children_statement->execute();
+    $dagr_statement->execute();
+    $db->commit();
+}
+
+function recursive_delete($guid, $db){
+    $dagr_statement = $db->prepare("delete from `DAGRS` where `DAGR_GUID` = ?");
+    $dagr_statement->bind_param("s", $guid);
+    $dagr_statement->execute();
+    $db->commit();
+}
+
+
+
+function delete_dagr($guid, $recursive){
+    $db = new mysqli("localhost", "root", "root", "mochila_db"); 
+    if($recurive){
+        recursive_delete($guid);
+    } else {
+        singular_delete($guid, $db);
+    }
+    $db->close();
+}
+
+
 $response_array = array();
 $guid = $_POST["guid"];
+$recursive = $_POST["guid"];
 
 if($guid != ""){
-    $db = new mysqli("localhost", "root", "root", "mochila_db");
-    $dagr_statement = $db->prepare("delete from `DAGRS` where `DAGR_GUID` =  ?");
-    $dagr_children_update = $db->prepare("update `DAGRS` set `DAGR_PARENT_GUID`=NULL where `DAGR_PARENT_GUID`= ?");
-    $dagr_children_list_update = $db->prepare("delete from `CHILD_DAGRS` where `PARENT_GUID` = ?");
-    $tags_statement = $db->prepare("delete from `TAGS` where `DAGR_GUID` = ?");
-    $dagr_statement->bind_param("s", $guid);
-    $tags_statement->bind_param("s", $guid);
-    $dagr_children_update->bind_param("s", $guid);
-    $dagr_children_list_update->bind_param("s", $guid);
-    $dagr_statement->execute();
-    $tags_statement->execute();
-    $dagr_children_update->execute();
-    $dagr_children_list_update->execute();
-    $db->commit();
-    
+    delete_dagr($guid);
     $response_array["status"] = "success";
     $response_array["guid"] = $guid;
     
 } else {
     $response_array["status"] = "error";
-    $response_array["guid"] = $guid;
-    
-    
+    $response_array["guid"] = $guid;        
 }
 
 header("Content Type: application/json");
--- tests/DeleteDagrTest.php
+++ tests/DeleteDagrTest.php
@@ -1,6 +1,8 @@
 <?php
+require_once("/home/nerraw/mochila/dagrDelete.php");
 
-class DeleteDagrTest extends PHPUnit_Framework_TestCase
+
+class DeleteDagrTest extends PHPUnit_Framework_TestCase    
 {
     
     public function setUp(){
@@ -52,21 +54,123 @@ class DeleteDagrTest extends PHPUnit_Framework_TestCase
         $statement->execute();
         $tag_stmnt->execute();
         
+        $guid = "4";
+        $title = "Sub container";
+        $date = "12-31-2013";
+        $size = 100;
+        $type = "parent";
+        $file_type = "parent";
+        $file_loc = NULL;
+        $author = "author";
+        $parent_guid = "1";
+        $tags = "tag3";
         
+        $statement->execute();
+        $tag_stmnt->execute();
         
+        $guid = "5";
+        $title = "subexample.odt";
+        $date = "12-31-2013";
+        $size = 100;
+        $type = "file";
+        $file_type = "odt";
+        $file_loc = "subexample.odt";
+        $author = "author";
+        $parent_guid = "4";
+        $tags = "tag3";
         
+        $statement->execute();
+        $tag_stmnt->execute();
         
+        $guid = "6";
+        $title = "Top Level";
+        $date = "12-31-2013";
+        $size = 100;
+        $type = "file";
+        $file_type = "odt";
+        $file_loc = "topLevel.odt";
+        $author = "author";
+        $parent_guid = NULL;
+        $tags = "tag3";
         
+        $statement->execute();
+        $tag_stmnt->execute();
+        $db->close();
     }
     
     public function tearDown(){
-        
+     
+        $db = new mysqli("localhost", "root", "root", "mochila_db");
+        $db->prepare("delete from `DAGRS`")->execute();
+        $db->prepare("truncate `TAGS`")->execute();
+        $db->close();
+        print "Tear down Called";
     }
     
-    public function testDeleteWorks(){
-        $this->assertTrue(true);
+    public function testSingularDeleteWorks(){
+        $db = new mysqli("localhost", "root", "root", "mochila_db");
+        singular_delete("1", $db);
+        $verify = $db->prepare("select `DAGR_GUID` from `DAGRS` where `DAGR_GUID`='1' or DAGR_PARENT_GUID='1'");
+        $verify->execute();
+        $verify->bind_result($res);
+        $results = array();
+        while($verify->fetch()){
+            $results[] = $res;
+        }
+        $this->assertEmpty($results, "Still contains DAGR");
         
+        $verify = $db->prepare("select `DAGR_GUID` from `DAGRS` where `DAGR_GUID`='2' or `DAGR_GUID`='3'");
+        $verify->execute();
+        $verify->bind_result($res);
+        $results = array();
+        while($verify->fetch()){
+            $results[] = $res;
+        }
+        $this->assertNotEmpty($results, "Children parents were not updated");
+        $this->assertContains("2", $results, "Children parents were not updated");
+        $this->assertContains("3", $results, "Children parents were not updated");
+        
+        
+        
+        $verify = $db->prepare("select `TAG_TITLE` from `TAGS` where `DAGR_GUID`='1'");
+        $verify->execute();
+        $verify->bind_result($res);
+        $results = array();
+        while($verify->fetch()){
+            $results[] = $res;
+        }
+        
+        $this->assertEmpty($results, "Tags were not deleted");
+        $db->close();
     }
+    
+    public function testRecursiveDeleteWorks(){
+        $db = new mysqli("localhost", "root", "root", "mochila_db");
+        recursive_delete("1", $db);
+        $verify = $db->prepare("select `DAGR_GUID` from `DAGRS`");
+        $verify->execute();
+        $verify->bind_result($res);
+        $results = array();
+        while($verify->fetch()){
+            $results[] = $res;
+        }
+        
+        $this->assertEquals(1, sizeof($results), "Contains extra DAGRS");
+        
+        $verify = $db->prepare("select `DAGR_GUID` from `TAGS`");
+        $verify->execute();
+        $verify->bind_result($res);
+        $results = array();
+        while($verify->fetch()){
+            $results[] = $res;
+        }
+        
+        $this->assertEquals(1, sizeof($results), "Contains extra DAGRS");
+        
+        
+        $db->close();
+    }
+    
     
     
 }
