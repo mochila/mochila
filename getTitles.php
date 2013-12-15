a87b7e9b431dcd1d417b7ab7c4f0e0875d5d2e86
Stephen <shaworth@terpmail.umd.edu>
2013-12-15 09:33:47 -0500

    edit getTitles.php to only return parent DAGR titles

--- getTitles.php
+++ getTitles.php
@@ -2,7 +2,7 @@
 // Get all the titles of DAGRS from the database
 $mysqli = new mysqli("localhost", "root", "dude1313", "mochila_db");
 
-$result = $mysqli->query("SELECT DAGR_TITLE FROM DAGRS");
+$result = $mysqli->query("SELECT DAGR_TITLE FROM DAGRS WHERE DAGR_TYPE='parent'");
 
 // Create an array from the result set
 $rows = array();
